3729279ac8c0d7109abb42f91eeac85de8e060ab
rysieko <rrx9506@gmail.com>
2025-11-08 16:00:47 +0100

    debuged

--- copy.sh
+++ copy.sh
@@ -1,10 +1,9 @@
-!#/bin/bash
-echo "Copy config? 
+#!/bin/bash
+echo "Copy config?" 
 select y in yes no
 do
 case $y in
 "yes") cp -rf ~/myhyprlanddocs/* ~/.config/hypr/* ;;
 "no") echo "if no than no" && exit ;;
-*) echo "chose an coreect option";;
 esac
 done
