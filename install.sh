409b2c726853307b8cbba0f14eac89be4eae53b3
rysieko <rrx9506@gmail.com>
2025-11-08 21:50:03 +0100

    fix syntax errors in install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ echo "do you want to install hyprland and copy dots?"
 select y in yes onlydots no 
 do
 case $y in
-"yes") x = $x + 1 ;;
-"onlydots") z = $z + 1 ;; 
+"yes") x=$((x + 1)) ;;
+"onlydots") z=$((z + 1)) ;; 
 "no") echo "Jak nie to nie" && exit ;;
 *) echo "chose an valid option" ;;
 esac
@@ -14,12 +14,13 @@ if [ $x -eq 1 ]
 then
 echo "Instaling"
 sudo pacman -Syu #updating system
-sudo pacman -S wireplumber pipewire sddm hyprpolkitagent xdg-desktop-portal-hyprland qt6-wayland noto-fonts hyprpaper cliphist iwgtk hyprland hyprlancher udiskie #instaling system packages
-sudo yay -S nordzy-cursors nordzy-hyprcursors ags-hyprpanel-git hyprls-git #installing AUR packages
+sudo pacman -S --noconfirm wireplumber pipewire sddm hyprpolkitagent xdg-desktop-portal-hyprland qt6-wayland noto-fonts hyprpaper cliphist iwgtk hyprland hyprlancher udiskie #instaling system packages
+sudo yay -S --noconfirm nordzy-cursors nordzy-hyprcursors ags-hyprpanel-git hyprls-git #installing AUR packages
 echo "coping dots"
 cp -r ~/rysiekohyprlanddocs/*.conf ~/.config/hypr/ #copying hyprland dots 
 hyprpanel useTheme "/usr/share/hyprpanel/themes/macchiato.json"
-#hyprctl setcursor nordzy-hyprcursors #TEMP
+hyprctl setcursor nordzy-hyprcursors #TEMP
+xcursor -set nordzy-cursors #TEMP
 fi
 if [ $z -eq 1 ]
 then
